c7aaa424b124bd66919c126b5349450944f654ed
brsoff <brsoff@gmail.com>
2014-08-24 12:45:16 -0400

    More styles

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,6 +3,8 @@ var app = {
     $('*[data-behavior="show-tweets"]').on('click', function () {
       var tweets = $(this).parent().find('.tweets');
       tweets.is(':visible') ? tweets.hide('fast') : tweets.show('fast');
+
+      $('html, body').animate({ scrollTop: $(this).offset().top - 40 }, 'fast');
     });
   }
 }
