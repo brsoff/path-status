7b87c7a00402c80649cfef1a94717f7b1536e15a
brsoff <brsoff@gmail.com>
2014-09-02 20:12:41 -0400

    Better-formatted specs

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -4,69 +4,83 @@ describe Tweet do
   subject { build(:tweet) }
   let(:tweets) { create_list(:tweet, 3, tweeted_at: Time.now.beginning_of_day) }
 
-  context "self.timeline" do
-    it "should return keys that correspond to specified days" do
-      timeline = Tweet.timeline(7)
+  describe "self.timeline" do
+    before {
+        tweets.second.tweeted_at = 10.days.ago
+        tweets.second.save!
 
-      expect(timeline.keys).to include(Date.today.timeline_date)
-      expect(timeline.keys).to include((Date.today - 6.days).timeline_date)
-      expect(timeline.keys).not_to include((Date.today - 7.days).timeline_date)
-    end
+        tweets.third.phrases = 'signal problem'
+        tweets.third.tweeted_at = 3.days.ago
+        tweets.third.save!
+      }
 
-    it "should return values as array of tweets with phrases tweeted on that day" do
-      tweets.second.tweeted_at = 10.days.ago
-      tweets.second.save!
+    context "given an integer" do
+      it "should return keys that correspond to specified days" do
+        expect(Tweet.timeline(7).keys).to include(Date.today.timeline_date)
+        expect(Tweet.timeline(7).keys).to include((Date.today - 6.days).timeline_date)
+        expect(Tweet.timeline(7).keys).not_to include((Date.today - 7.days).timeline_date)
+      end
 
-      tweets.third.phrases = 'signal problem'
-      tweets.third.tweeted_at = 3.days.ago
-      tweets.third.save!
-
-      timeline = Tweet.timeline(7)
-
-      expect(timeline[tweets.first.tweeted_at.to_date.timeline_date]).not_to include(tweets.first)
-      expect(timeline[tweets.second.tweeted_at.to_date.timeline_date]).to eq(nil)
-      expect(timeline[tweets.third.tweeted_at.to_date.timeline_date]).to include(tweets.third)
+      it "should return values as array of tweets with phrases tweeted on that day" do
+        expect(Tweet.timeline(7)[tweets.first.tweeted_at.to_date.timeline_date]).not_to include(tweets.first)
+        expect(Tweet.timeline(7)[tweets.second.tweeted_at.to_date.timeline_date]).to eq(nil)
+        expect(Tweet.timeline(7)[tweets.third.tweeted_at.to_date.timeline_date]).to include(tweets.third)
+      end
     end
   end
 
-  context "self.stats" do
-    it "should return hash of PHRASES with usage frequency" do
+  describe "self.stats" do
+    let(:timeline) { Tweet.timeline(7) }
+
+    before {
       tweets.first.phrases = 'signal problem, delay, signal construction'
       tweets.second.phrases = 'delay, police activity'
       tweets.third.phrases = 'signal construction, delay'
-
       tweets.each { |tweet| tweet.save! }
+    }
 
-      timeline = Tweet.timeline(7)
-      stats = Tweet.stats(timeline)
-
-      expect(stats['signal problem']).to eq(1)
-      expect(stats['delay']).to eq(3)
-      expect(stats['signal construction']).to eq(2)
-      expect(stats['track condition']).to eq(0)
+    context "given a timeline" do
+      it "should return hash of PHRASES with usage frequency" do
+        expect(Tweet.stats(timeline)['signal problem']).to eq(1)
+        expect(Tweet.stats(timeline)['delay']).to eq(3)
+        expect(Tweet.stats(timeline)['signal construction']).to eq(2)
+        expect(Tweet.stats(timeline)['track condition']).to eq(0)
+      end
     end
   end
 
-  context "record_phrases" do
-    it "should save a phrase if detected in the body of a tweet" do
-      subject.tweet_text = "Concerning the earlier signal problem, all PATH train service is resuming normal schedule."
-      subject.save!
+  describe "record_phrases" do
+    context "tweet contains one phrase" do
+      before {
+        subject.tweet_text = "Concerning the earlier signal problem, all PATH train service is resuming normal schedule."
+        subject.save!
+      }
 
-      expect(subject.phrases).to eq("signal problem")
+      it "should save the phrase" do
+        expect(subject.phrases).to eq("signal problem")
+      end
     end
 
-    it "should save multiple phrases if detected in the body of a tweet" do
-      subject.tweet_text = "Due to a signal problem, all PATH train service has been suspended."
-      subject.save!
+    context "tweet contains multiple phrases" do
+      before {
+        subject.tweet_text = "Due to a signal problem, all PATH train service has been suspended."
+        subject.save!
+      }
 
-      expect(subject.phrases).to eq("suspended, signal problem")
+      it "should save multiple phrases" do
+        expect(subject.phrases).to eq("suspended, signal problem")
+      end
     end
 
-    it "should save as nil if no phrases are detected" do
-      subject.tweet_text = "Unrelated tweet"
-      subject.save!
+    context "tweet does not contain any phrases" do
+      before {
+        subject.tweet_text = "Unrelated tweet"
+        subject.save!
+      }
 
-      expect(subject.phrases).to eq(nil)
+      it "should save as nil if no phrases are detected" do
+        expect(subject.phrases).to eq(nil)
+      end
     end
   end
 end
